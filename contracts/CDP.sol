264e6ebebf74f6cc2dd7ebee112e3dea32ee7918
hrishikeshio <hrishikesh911@gmail.com>
2019-08-03 18:30:19 +0530

    open zeppelin

--- contracts/CDP.sol
+++ contracts/CDP.sol
@@ -1,7 +1,17 @@
-functions
-deposit with eth here
-fetch price from oracle oracle.sol
-mint appropriate erc 20 token erc20 factory.sol
-give token to user this
-function to burn token to return deposit this
-function to liquidate (optional) this
+// functions
+// deposit with eth here
+// fetch price from oracle oracle.sol
+// mint appropriate erc 20 token erc20 factory.sol
+// give token to user this
+// function to burn token to return deposit this
+// function to liquidate (optional) this
+
+
+contract CDP {
+	function mint(string url, string selector)  public payable {
+		uint256 val = msg.value;
+		address sender = msg.sender;
+
+	}
+
+}
