7cc2ef435f4fe30953c1d341ba8ccd2e9b2ed389
hrishikeshio <hrishikesh911@gmail.com>
2019-08-03 18:56:43 +0530

    fi errors

--- contracts/CDP.sol
+++ contracts/CDP.sol
@@ -3,14 +3,25 @@
 // fetch price from oracle oracle.sol
 // mint appropriate erc 20 token erc20 factory.sol
 // give token to user this
+pragma solidity ^0.5.8;
 // function to burn token to return deposit this
 // function to liquidate (optional) this
-
-
+import "./SimpleToken.sol";
+import "./Oracle.sol";
 contract CDP {
-	function mint(string url, string selector)  public payable {
+	mapping (bytes32 => address) public contracts;
+	Oracle oracle = Oracle(0x0);
+	function createRequest(string memory url, string memory selector) public payable {
 		uint256 val = msg.value;
 		address sender = msg.sender;
+		bytes32 id = keccak256(abi.encode(url, selector));
+		oracle.request(id, url, selector);
+	}
+
+	function mint() public
+	//only oracle 
+	{
+		SimpleToken st = new SimpleToken();
 
 	}
 
--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,11 +1,12 @@
+pragma solidity ^0.5.8;
 contract Oracle {
 	event Requested(bytes32 id, string url, string selector);
-	function request (bytes32 id, string url, string selector) public {
+	function request (bytes32 id, string memory url, string memory selector) public {
 		emit Requested(id, url, selector);
 	}
 
 	event Fulfilled (bytes32 id, uint256 value);
-	function fulfil (bytes32 id, uint256 value) {
+	function fulfil (bytes32 id, uint256 value) public {
 		emit Fulfilled(id, value);
 	}
 }
