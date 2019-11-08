4d0d849035a718e2c3e21af88645a7591a37de01
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2019-11-08 12:00:53 +0530

    disable navigation toggle for mobile

--- src/layouts/Base.vue
+++ src/layouts/Base.vue
@@ -9,9 +9,9 @@
       </router-link>
     </div>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
+    </button> -->
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <!-- <a class="nav-item nav-link active" href="#">Home </a> -->
